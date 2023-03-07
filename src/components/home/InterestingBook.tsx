f16960625f6ddcc896b0393c163f82823731cd54
ted-han <taekyung28h@gmail.com>
2023-03-07 23:32:20 +0900

    Feat: [#3] 컴포넌트화 - 관심도서 추천 섹션

---
+++ src/components/home/InterestingBook.tsx
@@ -0,0 +1,64 @@
+import styles from "@/styles/Home.module.scss";
+import TextGothic from "@/components/common/TextGothic";
+import BookBox from "@/components/common/BookBox";
+import { bookData } from "@/type";
+
+interface InterestingBook {
+  data: bookData[];
+}
+
+export default function InterestingBook({ data }: InterestingBook) {
+  return (
+    <section className={styles.interestingSection}>
+      <div className={styles.recomendBox}>
+        <TextGothic
+          text="suxxxmin님, 관심 도서를 추천할게요."
+          fontWeight={700}
+          fontSize={20}
+          lineHeight={28}
+          color="#000000"
+          style={{
+            display: "block",
+            marginBottom: "24px",
+          }}
+        />
+        <div className={styles.category}>
+          <TextGothic
+            text="자기계발"
+            fontWeight={700}
+            fontSize={40}
+            lineHeight={64}
+            color="#1a1a1a"
+            style={{
+              display: "block",
+              marginBottom: "12px",
+            }}
+          />
+          <TextGothic
+            text="건강/의학"
+            fontWeight={700}
+            fontSize={40}
+            lineHeight={64}
+            color="#B3B3B3"
+            style={{
+              display: "block",
+              marginBottom: "12px",
+            }}
+          />
+          <TextGothic
+            text="인문/사회"
+            fontWeight={700}
+            fontSize={40}
+            lineHeight={64}
+            color="#B3B3B3"
+            style={{
+              display: "block",
+              marginBottom: "12px",
+            }}
+          />
+        </div>
+      </div>
+      <BookBox data={data} />
+    </section>
+  );
+}
